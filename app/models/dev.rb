dba39fa9db76737faecf553062bb258fc8a2d8ff
Laura <lnkeat@gmail.com>
2022-01-18 22:14:12 -0700

    Done.

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,17 @@
 class Dev < ActiveRecord::Base
+  has_many :freebies
+  has_many :companies, through: :freebies
+
+  def received_one?(item_name)
+    self.freebies.exists?(:item_name => item_name)
+  end
+
+  def give_away(dev, freebie)
+    if self.freebies.includes(freebie)
+      freebie.update(dev:dev)
+    else
+      "you can't give this away because it's not yours"
+    end
+  end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,11 +11,14 @@ Dev.create(name: "Mr. Meseeks")
 Dev.create(name: "Gazorpazop")
 
 puts "Creating freebies..."
+companies = Company.all.map { |c| c.id }
+devs= Dev.all.map { |d| d.id }
 
-# ***************************************************************
-# * TODO: create freebies! Remember, a freebie belongs to a dev *
-# * and a freebie belongs to a company.                         *
-# ***************************************************************
-# Create freebies Here
+Freebie.create(item_name: "first freeb item", value: 10, dev_id: devs[0], company_id: companies[3] )
+Freebie.create(item_name: "second freeb item", value: 23, dev_id: devs[1], company_id: companies[2])
+Freebie.create(item_name: "third freeb item", value: 36, dev_id: devs[2], company_id: companies[1])
+Freebie.create(item_name: "fourth freeb item", value: 41, dev_id: devs[3], company_id: companies[0])
+Freebie.create(item_name: "another freeb item", value: 10, dev_id: devs[1], company_id: companies[1] )
+
 
 puts "Seeding done!"
